db6e177472fc658d55a54dd3892343dc7a561f03
Saikerang <Gaymaster@non.com>
2023-12-19 17:08:30 +0700

    Completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 
 using namespace std;
 
@@ -9,18 +10,30 @@ int main(){
 
     int count = 0;
     float sum = 0;
+    float num = 0;
+    float SD;
+    float x;
     
     string text;
-    ifstream source("score.txt")
+    ifstream source("score.txt");
+
     while (getline(source,text))
-    {
+    {   
+        x = atof(text.c_str());
         sum+=atof(text.c_str());
         count++;
+        num += pow(x,2);
+
     }
-    cout << "Number of data = ";
+   
+    SD = sqrt((num/count)-pow((sum/count),2));
+
+    
+    
+    cout << "Number of data = "<<count<<'\n';
     cout << setprecision(3);
-    cout << "Mean = ";
-    cout << "Standard deviation = ";
+    cout << "Mean = "<<sum/count<<'\n';
+    cout << "Standard deviation = "<<SD<<'\n';
     source.close();
     return 0;
 }
