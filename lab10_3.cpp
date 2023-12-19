339f020123457c7673217f35eab6dd7e90318773
Saikerang <Gaymaster@non.com>
2023-12-19 16:31:41 +0700

    completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+
+    int count = 0;
+    float sum = 0;
+    
+    string text;
+    ifstream source("score.txt")
+    while (getline(source,text))
+    {
+        sum+=atof(text.c_str());
+        count++;
+    }
+    cout << "Number of data = ";
+    cout << setprecision(3);
+    cout << "Mean = ";
+    cout << "Standard deviation = ";
+    source.close();
+    return 0;
+}
